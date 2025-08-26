35ad2d3aeb2068ddf46b248f82d51997ef55d1f4
Gabriel <gabriel.gracilianomb@gmail.com>
2025-08-25 21:17:40 -0300

    Create fake AudioManager

---
+++ src/com/gracilianoog/gof/facade/AudioManager.java
@@ -0,0 +1,15 @@
+package com.gracilianoog.gof.facade;
+
+public class AudioManager {
+    public void playMusic() {
+        System.out.println("Playing game music...");
+    }
+
+    public void stopMusic() {
+        System.out.println("Stopping current music...");
+    }
+
+    public void pauseMusic() {
+        System.out.println("Pausing current music...");
+    }
+}
