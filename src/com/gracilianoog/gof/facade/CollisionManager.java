9728679f024b39b15ad4dbb1756fb019857bdff2
Gabriel <gabriel.gracilianomb@gmail.com>
2025-08-25 21:20:46 -0300

    Create fake CollisionManager

---
+++ src/com/gracilianoog/gof/facade/CollisionManager.java
@@ -0,0 +1,7 @@
+package com.gracilianoog.gof.facade;
+
+public class CollisionManager {
+    public void detectCollision() {
+        System.out.println("Detecting collision between object A and B");
+    }
+}
