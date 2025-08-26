f222db8814c614cce02f44da1087eeee8566ef32
Gabriel <gabriel.gracilianomb@gmail.com>
2025-08-25 21:28:18 -0300

    Instance a new engine for testing purposes

---
+++ src/com/gracilianoog/gof/facade/Main.java
@@ -0,0 +1,10 @@
+package com.gracilianoog.gof.facade;
+
+public class Main {
+    public static void main(String[] args) {
+        Engine engine = new Engine();
+
+        engine.startGame();
+        engine.stopGame();
+    }
+}
